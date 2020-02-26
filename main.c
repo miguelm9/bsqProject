496a9e4390c5356265acdd970aa80d119b743b96
Miguel Martin <MiguelMartinn9@gmail.com>
2020-02-26 19:27:02 +0100

    todo bien brother

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mmartin <mmartin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/24 14:19:18 by mmartin           #+#    #+#             */
-/*   Updated: 2020/02/26 18:35:27 by mmartin          ###   ########.fr       */
+/*   Updated: 2020/02/26 19:05:16 by mmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,38 @@
 int	main(int argc, char *argv[])
 {
 	int i;
-	char	buf;
-	int		fd;
 
 	i = 1;
-	if (argc == 1)
-	{
-		read_from_console();
-	}
-	while (i < argc)
-	{
-		char	array_mapa[get_lines(open_file(argv[i]))]
-		[get_char(open_file(argv[i]))];
-		if (is_everything_good(argv[i]))
+	// if (argc == 1)
+	// {
+	// 	read_from_console();
+	// 	char	array_mapa[get_lines(open_file("console.txt"))]
+	// 	[get_char(open_file("console.txt"))];
+	// 	if (is_everything_good("console.txt"))
+	// 	{
+	// 		map_creator(open_file("console.txt"), array_mapa);
+	// 		find_submatrix(open_file("console.txt"), array_mapa);
+	// 		write(1, "\n", 1);
+	// 	}
+	// 	else
+	// 		write(1, "map error\n", 11);
+	// }
+	// else
+	// {
+		while (i < argc)
 		{
-			map_creator(open_file(argv[i]), array_mapa);
-			find_submatrix(open_file(argv[i]), array_mapa);
-			write(1, "\n", 1);
+			char	array_mapa[get_lines(open_file(argv[i]))]
+			[get_char(open_file(argv[i]))];
+			if (is_everything_good(argv[i]))
+			{
+				map_creator(open_file(argv[i]), array_mapa);
+				find_submatrix(open_file(argv[i]), array_mapa);
+				write(1, "\n", 1);
+			}
+			else
+				write(1, "map error\n", 11);
+			i++;
 		}
-		else
-			write(1, "map error\n", 11);
-		i++;
-	}
+	//}
 	return (0);
 }
--- user_read.c
+++ user_read.c
@@ -6,18 +6,18 @@
 /*   By: mmartin <mmartin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 18:11:08 by mmartin           #+#    #+#             */
-/*   Updated: 2020/02/26 18:34:42 by mmartin          ###   ########.fr       */
+/*   Updated: 2020/02/26 19:06:04 by mmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-void read_from_console(void)
+void	read_from_console(void)
 {
 	char	buf;
 	int		fd;
 
-	fd = open("console.txt", O_RDWR | O_CREAT);
+	fd = open("console.txt", O_CREAT | O_RDWR, 0644);
 	while (read(0, &buf, 1))
 		write(fd, &buf, 1);
 	close(fd);
